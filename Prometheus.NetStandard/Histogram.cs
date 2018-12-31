6489b8064b3b97107711b317913e3e5412bb74f9
Chris Mannix <chris.mannix@flextrade.com>
2018-12-31 12:20:58 +0000

    Add HistogramBucket functions

--- Prometheus.NetStandard/Histogram.cs
+++ Prometheus.NetStandard/Histogram.cs
@@ -107,5 +107,63 @@ namespace Prometheus
         }
 
         public void Publish() => Unlabelled.Publish();
+        
+        // From https://github.com/prometheus/client_golang/blob/master/prometheus/histogram.go
+        /// <summary>  
+        ///  Creates '<paramref name="count"/>' buckets, where the lowest bucket has an
+        ///  upper bound of '<paramref name="start"/>' and each following bucket's upper bound is '<paramref name="factor"/>'
+        ///  times the previous bucket's upper bound.
+        /// 
+        ///  The function throws if '<paramref name="count"/>' is 0 or negative, if '<paramref name="start"/>' is 0 or negative,
+        ///  or if '<paramref name="factor"/>' is less than or equal 1.
+        /// </summary>
+        /// <param name="start">The upper bound of the lowest bucket. Must be positive.</param>
+        /// <param name="factor">The factor to increase the upper bound of subsequent buckets. Must be greater than 1.</param>
+        /// <param name="count">The number of buckets to create. Must be positive.</param>
+        public static double[] ExponentialBuckets(double start, double factor, int count)
+        {
+            CheckArgument(count > 0, $"{nameof(ExponentialBuckets)} needs a positive {nameof(count)}");
+            CheckArgument(start > 0, $"{nameof(ExponentialBuckets)} needs a positive {nameof(start)}");
+            CheckArgument(factor > 1, $"{nameof(ExponentialBuckets)} needs a {nameof(factor)} greater than 1");
+
+            var buckets = new double[count];
+
+            for (var i = 0; i < buckets.Length; i++)
+            {
+                buckets[i] = start;
+                start *= factor;
+            }
+
+            return buckets;
+        }
+        
+        // From https://github.com/prometheus/client_golang/blob/master/prometheus/histogram.go
+        /// <summary>  
+        ///  Creates '<paramref name="count"/>' buckets, where the lowest bucket has an
+        ///  upper bound of '<paramref name="start"/>' and each following bucket's upper bound is the upper bound of the
+        ///  previous bucket, incremented by '<paramref name="width"/>'
+        /// 
+        ///  The function throws if '<paramref name="count"/>' is 0 or negative.
+        /// </summary>
+        /// <param name="start">The upper bound of the lowest bucket. Must be positive.</param>
+        public static double[] LinearBuckets(double start, double width, int count)
+        {
+            CheckArgument(count > 0, $"{nameof(LinearBuckets)} needs a positive {nameof(count)}");
+
+            var buckets = new double[count];
+
+            for (var i = 0; i < buckets.Length; i++)
+            {
+                buckets[i] = start;
+                start += width;
+            }
+
+            return buckets;
+        }
+
+        private static void CheckArgument(bool condition, string message)
+        {
+            if (!condition) throw new ArgumentException(message);
+        }
     }
 }
